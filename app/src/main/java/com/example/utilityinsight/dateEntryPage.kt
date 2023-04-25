b1a6bae7be33a5ce0dbf5856d4b8d9f84fd81734
IT21208294 <IT21208294@my.sliit.lk>
2023-04-25 18:08:38 +0530

    updatedGas

---
+++ app/src/main/java/com/example/utilityinsight/dateEntryPage.kt
@@ -0,0 +1,11 @@
+package com.example.utilityinsight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dateEntryPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gas_date_entry_page)
+    }
+}
