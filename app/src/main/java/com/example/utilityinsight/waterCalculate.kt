e3a8090eb84e4cbe67fa567d8e2ec488562015e0
AmadeeNisalasara <it21209666@my.sliit.lk>
2023-05-02 17:20:31 +0530

    Updated water calculate

--- app/src/main/java/com/example/utilityinsight/waterCalculate.kt
+++ app/src/main/java/com/example/utilityinsight/waterCalculate.kt
@@ -3,7 +3,6 @@ package com.example.utilityinsight
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.*
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -14,9 +13,11 @@ class waterCalculate : AppCompatActivity() {
     private lateinit var waccnumber: EditText
     private lateinit var wdays: EditText
     private lateinit var wunits: EditText
+    private lateinit var waccname: EditText
     private lateinit var btncal: Button
+    private lateinit var btnventries: Button
     val charges = arrayOf("Fixed Charge(Units)","0-25 = 100.00(LKR)", "26-30 = 200.00(LKR)","31-40 = 400.00(LKR)","41-50 = 650.00(LKR)","51-75 = 1000.00(LKR)","Above 75 = 1600.00(LKR)")
-
+    private lateinit var totcal: TextView
 
 
     private var db = Firebase.firestore
@@ -27,55 +28,113 @@ class waterCalculate : AppCompatActivity() {
 
         //assigning variables to id
         waccnumber = findViewById(R.id.w_acc_number)
+        waccname = findViewById(R.id.w_acc_name)
         wdays = findViewById(R.id.w_number_of_days)
         wunits = findViewById(R.id.w_number_of_units)
         btncal = findViewById(R.id.w_home_calculate_btn)
-
-        //implementing spinner
-        val spinner = findViewById<Spinner>(R.id.w_fixed_charge)
-        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, charges)
-        spinner.adapter = arrayAdapter
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                Toast.makeText(
-                    applicationContext,
-                    "Selected category is " + charges[p2],
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-
-
-            }
-        }
-
+        btnventries = findViewById(R.id.w_home_store)
+        totcal = findViewById(R.id.viewtotal)
 
         btncal.setOnClickListener {
-            val accountNumber = waccnumber.text.toString().trim()
-            val numberOfDays = wdays.text.toString().trim()
-            val numberOfUnits = wunits.text.toString().trim()
 
-            val userMap = hashMapOf(
-                "Account Number" to accountNumber,
-                "Number of days" to numberOfDays,
-                "Number of units" to numberOfUnits,
-            )
+            val waterunitText = wunits.text.toString().trim()
+            val waterdaysText = wdays.text.toString().trim()
+            val wateraccountNumber = waccnumber.toString().trim()
+            val wateraccountName = waccname.toString().trim()
 
-            db.collection("wcalculate").document().set(userMap)
-                .addOnSuccessListener {
-                    waccnumber.text.clear()
-                    wdays.text.clear()
-                    wunits.text.clear()
-                    Toast.makeText(this, "Calculate successfully",Toast.LENGTH_SHORT).show()
-                    val i = Intent(this, waterHome::class.java)
-                    startActivity(i)
-                    finish()
-                }
+            // Check if any of the fields are empty
+            if (waterunitText.isEmpty() || waterdaysText.isEmpty() || wateraccountNumber.isEmpty() || wateraccountName.isEmpty()) {
+                Toast.makeText(applicationContext, "Please fill in all the fields", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener }
+
+
+
+            val waterunit: Int
+            try {
+                waterunit = waterunitText.toInt()
+            } catch (e: NumberFormatException) {
+                Toast.makeText(applicationContext, "Invalid input for units", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+
+            val waterdays: Int
+            try {
+                waterdays = waterdaysText.toInt()
+            } catch (e: NumberFormatException) {
+                Toast.makeText(applicationContext, "Invalid input for days", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+
+            var wnormalcharge = 0
+            var wfixedcharge = 0
+            var wtotal = 0
+
+            if (waterunit < 25) {
+                wfixedcharge = 100
+                wnormalcharge = waterunit * waterdays
+                wtotal = wfixedcharge + wnormalcharge
+            } else if (waterunit < 30) {
+                wfixedcharge = 200
+                wnormalcharge = waterunit * waterdays
+                wtotal = wfixedcharge + wnormalcharge
+            } else if (waterunit < 40) {
+                wfixedcharge = 400
+                wnormalcharge = waterunit * waterdays
+                wtotal = wfixedcharge + wnormalcharge
+            } else if (waterunit < 50) {
+                wfixedcharge = 650
+                wnormalcharge = waterunit * waterdays
+                wtotal = wfixedcharge + wnormalcharge
+            } else if (waterunit < 75) {
+                wfixedcharge = 1000
+                wnormalcharge = waterunit * waterdays
+                wtotal = wfixedcharge + wnormalcharge
+            } else {
+                wfixedcharge = 1600
+                wnormalcharge = (waterunit * waterdays)
+                wtotal = wfixedcharge + wnormalcharge
+            }
+
+            totcal.append("Total Payable Amount: $wtotal\n")
+            totcal.append("Bill Period: $waterdays\n")
+            totcal.append("Fixed Charge: $wfixedcharge")
+
+        }
+
+            btnventries.setOnClickListener{
+                val accountNumber = waccnumber.text.toString().trim()
+                val accountName = waccname.text.toString().trim()
+                val numberOfDays = wdays.text.toString().trim()
+                val numberOfUnits = wunits.text.toString().trim()
+                val wtotamount = totcal.text.toString().trim()
+
+
+                val userMap = hashMapOf(
+                    "Account Number" to accountNumber,
+                    "Account Name" to accountName,
+                    "Number of days" to numberOfDays,
+                    "Number of units" to numberOfUnits,
+                    "Total Amount" to wtotamount
+
+                    )
+
+                db.collection("wcalculate").document().set(userMap)
+                    .addOnSuccessListener {
+                        waccnumber.text.clear()
+                        waccname.text.clear()
+                        wdays.text.clear()
+                        wunits.text.clear()
+                        Toast.makeText(this, "Calculate successfully",Toast.LENGTH_SHORT).show()
+                        val i = Intent(this, waterHome::class.java)
+                        startActivity(i)
+                        finish()
+                    }
+            }
+
+
 
         }
 
     }
-}
