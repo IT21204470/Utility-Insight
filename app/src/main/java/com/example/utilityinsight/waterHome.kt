e190b82f850327d7613ca039032d774f4ad53149
Hemsith Deesara <99312182+IT21204470@users.noreply.github.com>
2023-05-06 03:24:06 +0530

    updated files

--- app/src/main/java/com/example/utilityinsight/waterHome.kt
+++ app/src/main/java/com/example/utilityinsight/waterHome.kt
@@ -13,13 +13,9 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
 class waterHome : AppCompatActivity() {
-
-
-    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_water_home)
-
 
         val waterCalCardView = findViewById<CardView>(R.id.w_cardview1)
         waterCalCardView.setOnClickListener {
@@ -28,7 +24,7 @@ class waterHome : AppCompatActivity() {
         }
         val waterPayCardView = findViewById<CardView>(R.id.w_cardview2)
         waterPayCardView.setOnClickListener {
-            val intent = Intent(this, paymentMethod::class.java)
+            val intent = Intent(this, electricityPayment::class.java)
             startActivity(intent)
         }
         val waterAddCardView = findViewById<CardView>(R.id.w_cardview3)
@@ -36,9 +32,21 @@ class waterHome : AppCompatActivity() {
             val intent = Intent(this, addConnection::class.java)
             startActivity(intent)
         }
-        val waterTipsCardView = findViewById<CardView>(R.id.w_cardview4)
+        val waterEntryCardView = findViewById<CardView>(R.id.w_cardview4)
+        waterEntryCardView.setOnClickListener {
+            val intent = Intent(this, waterEntries::class.java)
+            startActivity(intent)
+        }
+
+        val waterTipsCardView = findViewById<CardView>(R.id.w_cardview5)
         waterTipsCardView.setOnClickListener {
             val intent = Intent(this, waterShowtips::class.java)
+            startActivity(intent)
+        }
+
+        val waterNotifyCardView = findViewById<CardView>(R.id.w_cardview6)
+        waterNotifyCardView.setOnClickListener {
+            val intent = Intent(this, displayNotifications::class.java)
             startActivity(intent)
         }
 
