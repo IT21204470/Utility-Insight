1e78a4814fe3de68cd3f4361f5ebf75db711bf31
IT21207822 <it21207822@my.sliit.lk>
2023-05-05 09:33:20 +0530

    Completed User Side

---
+++ app/src/main/java/com/example/utilityinsight/ForgotPassword.kt
@@ -0,0 +1,12 @@
+package com.example.utilityinsight
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgotPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_front)
+    }
+}
